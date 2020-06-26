08bcb35ecd71b6c91cef56bded66184fe4b8e910
mozewillo <aleksmozwillo@gmail.com>
2020-06-26 20:34:35 +0200

    basic table with challenges

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from challenges.models import Challenge
+
+
+def showChallenges(request):
+    challenges_list = Challenge.objects.all()
+    return render(request, 'challenges.challenges.html', locals())
+
